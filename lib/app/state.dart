4c3090df53e798aed3de8842e8a337312dee07fb
Tony Yip <tony@opensource.hk>
2018-03-18 16:42:28 +0800

    Integrate firebase analytics

--- lib/app/state.dart
+++ lib/app/state.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_analytics/firebase_analytics.dart' show FirebaseAnalytics;
+import 'package:firebase_analytics/observer.dart' show FirebaseAnalyticsObserver;
+
 import 'app.dart' show HKOSConClientApp;
 import 'const.dart' show AppTitle, PrimaryColor, SecondaryColor;
 import 'pages/home/page.dart' show HomePage;
@@ -7,8 +10,11 @@ import 'Container.dart' show AppContainer;
 
 class HKOSConClientAppState extends State<HKOSConClientApp> {
 
+  final FirebaseAnalytics analytics = new FirebaseAnalytics();
+
   @override
   Widget build(BuildContext context) {
+    final FirebaseAnalyticsObserver observer = new FirebaseAnalyticsObserver(analytics: this.analytics);
     ThemeData theme = new ThemeData(
       brightness: Brightness.light,
       primaryColor: PrimaryColor,
@@ -20,6 +26,9 @@ class HKOSConClientAppState extends State<HKOSConClientApp> {
       routes: <String, WidgetBuilder>{
         '/': (BuildContext context) => new AppContainer(new HomePage()),
       },
+      navigatorObservers: [
+        observer,
+      ],
     );
   }
 
