84fed7ea8d8ebf9c25807e480cd009a659e6b744
Tony Yip <tonyhhyip@gmail.com>
2018-05-16 07:42:11 +0800

    Add route

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,17 +6,19 @@ import 'theme.dart' show AppTheme;
 import 'container.dart' show AppContainer;
 import 'router/router.dart' show ContainerBuilder, Router;
 import 'pages/home/page.dart' show HomePage;
+import 'pages/schedule/page.dart' show SchedulePage;
+
+final routes = <String, WidgetBuilder>{
+  '/': (BuildContext context) => new HomePage(),
+  '/schedule': (BuildContext context) => new SchedulePage(),
+};
+
+final ContainerBuilder builder = (BuildContext context, Widget child) => new AppContainer(child);
 
 class HKOSConClientApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var routes = <String, WidgetBuilder>{
-      '/': (BuildContext context) => new HomePage(),
-    };
-
-    ContainerBuilder builder = (BuildContext context, Widget child) => new AppContainer(child);
-
-    var router = new Router(
+    final router = new Router(
       routes: routes,
       builder: builder,
     );
